e28d785cd029e52d43a58da2979ad74e422d0eab
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-15 15:19:05 -0600

    commit tree e8ab3867438f5baaf63b8ac670f83a47705b3fd9

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,69 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  <<-SQL
+    SELECT books.title, books.year
+    FROM books
+    JOIN series
+    ON books.series_id = series.id
+    WHERE books.series_id = 1
+    ORDER BY books.year;
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+ <<-SQL
+    SELECT characters.name, characters.motto
+    FROM characters
+    ORDER BY length(characters.motto) DESC LIMIT 1;
+ SQL
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  <<-SQL
+    SELECT characters.species, COUNT(characters.species)
+    FROM characters
+    GROUP BY characters.species
+    ORDER BY COUNT(characters.species)
+    DESC LIMIT 1;
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  <<-SQL
+    SELECT authors.name, subgenres.name
+    FROM authors
+    JOIN series
+    ON series.author_id = authors.id
+    JOIN subgenres
+    ON series.subgenre_id = subgenres.id
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+    SELECT series.title
+    FROM series
+    JOIN authors
+    ON authors.id = series.author_id
+    JOIN characters
+    ON characters.author_id = authors.id
+    WHERE characters.species = 'human'
+    GROUP BY characters.species
+    ORDER BY COUNT(characters.species)
+    DESC LIMIT 1
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+ <<-SQL
+    SELECT characters.name, COUNT(*) AS book_count
+    FROM character_books
+    JOIN books
+    ON character_books.book_id = books.id
+    JOIN characters
+    ON character_books.character_id = characters.id
+    GROUP BY characters.name 
+    ORDER BY book_count DESC, characters.name
+
+ SQL
 end
